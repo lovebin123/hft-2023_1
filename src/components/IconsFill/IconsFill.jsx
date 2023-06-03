ede7f7a5f778ce0de600248313be534e2b08d74c
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-06-03 21:59:55 +0530

    minor

---
+++ src/components/IconsFill/IconsFill.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './IconsFill.css'
+const IconsFil = ({ children }) => {
+  return (
+    <div>
+      {children}
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+};
+
+export default IconsFil;
