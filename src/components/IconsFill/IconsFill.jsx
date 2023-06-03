8e2e5e3d19f6752e720c76b86734e1a0cc6f2661
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-06-03 23:49:36 +0530

    y ppt

--- src/components/IconsFill/IconsFill.jsx
+++ src/components/IconsFill/IconsFill.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './IconsFill.css'
-const IconsFil = ({ children }) => {
+const IconsFill = ({ children }) => {
   return (
     <div>
       {children}
@@ -11,4 +11,4 @@ const IconsFil = ({ children }) => {
   );
 };
 
-export default IconsFil;
+export default IconsFill;
