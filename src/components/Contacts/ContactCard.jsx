fd8b2043cd2626b26ffa6237ea4eee9250d6a5ba
Sona-Martin <sonarosemartin@gmail.com>
2023-06-04 15:10:02 +0530

    contacts

---
+++ src/components/Contacts/ContactCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FiPhoneCall, FiLinkedin, FiMail } from "react-icons/fi";
+
+function ContactCard({ name, designation, image, email, phone, linkedIn }) {
+  return (
+    <div data-aos="zoom-in" data-aos-delay="150" className="contactCard">
+    
+      <div className="contact_img">
+        <img src={image} alt="contacting" />
+      </div>
+      <h2>{name}</h2>
+      <h3>{designation}</h3>
+      <div className="contact_social">
+        <a href={`tel:${phone}`} target="_blank" rel="noreferrer">
+          <FiPhoneCall className="contact_social_icon" />
+        </a>
+        <a href={`mailto:${email}`} target="_blank" rel="noreferrer">
+          <FiMail className="contact_social_icon" />
+        </a>
+        <a href={linkedIn} target="_blank" rel="noreferrer">
+          <FiLinkedin className="contact_social_icon" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default ContactCard;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-import './Contacts.css'
-const Contacts = () => {
-  return (
-    <div>
-        <h1 className='section__title'>Contacts</h1>
-    </div>
-  )
-}
+import React from "react";
+import "./Contacts.css";
+import persona from "../../assets/png/persona.png";
+import persona2 from "../../assets/png/persona2.png";
+import ContactCard from "./ContactCard";
+const contactsData = [
+  {
+    id: 1,
+    name: "Nasreen K Basheer",
+    designation: "Chairperson",
+    image: persona,
+    phone: "9605204433",
+    email: "nasreenkbasheer.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/nasreen-k-basheer-6795331a5/",
+  },
+  {
+    id: 2,
+    name: "Hemanth Sagar J C",
+    designation: "General Secretary",
+    image: persona2,
+    phone: "7012910210",
+    email: "hemanthsagar.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/hemanth-sagar-j-c-292a98195",
+  },
+  {
+    id: 3,
+    name: "Sebin Davis",
+    designation: "Joint Secretary",
+    image: persona,
+    phone: "8921212340",
+    email: "sebindavis.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
+  },
+  {
+    id: 4,
+    name: "Sania Maria Sabu",
+    designation: "Social Initiatives Manager",
+    image: persona2,
+    phone: "7306928074",
+    email: "saniamariasabu.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
+  },
+];
 
-export default Contacts
+const Contact = () => {
+  return (
+    <div className="contact" id="contact">
+      <div className="contact__container">
+        <h1 className="section__title">Contact us</h1>
+
+        <div className="contact__cards">
+          {contactsData.map((contact) => (
+            <ContactCard
+              key={contact.id}
+              name={contact.name}
+              designation={contact.designation}
+              image={contact.image}
+              phone={contact.phone}
+              email={contact.email}
+              linkedIn={contact.linkedIn}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
