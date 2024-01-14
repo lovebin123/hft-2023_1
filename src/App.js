9d871c87f55bedb8e8a87e97727ad286b41c5a5c
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-01-14 19:13:27 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { Suspense } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainPage } from "./pages";
 import ScrollToTop from "./utils/ScrollToTop";
 import Loader from "./components/Loader/Loader";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []); // Empty dependency array ensures the effect runs only once
+
   return (
     <div className="App">
       <BrowserRouter>
         <Suspense fallback={<Loader />}>
           <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-          </Routes>
+          {loading ? (
+            <Loader />
+          ) : (
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+            </Routes>
+          )}
         </Suspense>
       </BrowserRouter>
     </div>
