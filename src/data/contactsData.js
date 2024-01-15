280296fc8138891c2236938ca09e93e7f32ba04b
Sona-Martin <sonarosemartin@gmail.com>
2024-01-15 12:29:02 +0530

    bounties

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,64 +1,63 @@
-import vishnu from '../assets/jpg/vishnu.png'
-import mathew from '../assets/jpg/mathew.png'
-import mayon from '../assets/jpg/mayon.png'
-import abhishek from '../assets/jpg/abhishek.png'
-import goutham from '../assets/jpg/goutham.png'
-import vaidyanath from '../assets/jpg/vaidyanath.png'
+import vishnu from "../assets/jpg/vishnu.png";
+import mathew from "../assets/jpg/mathew.png";
+import mayon from "../assets/jpg/mayon.png";
+import abhishek from "../assets/jpg/abhishek.png";
+import goutham from "../assets/jpg/goutham.png";
+import vaidyanath from "../assets/jpg/vaidyanath.png";
 
 export const contactsData = [
-    {
-      id: 1,
-      name: "Mathew Davis",
-      designation: "Chairperson",
-      image:mathew,
-      phone: "9605204433",
-      email: "nasreenkbasheer.mec@gmail.com",
-      linkedIn: "https://www.linkedin.com/in/nasreen-k-basheer-6795331a5/",
-    },
-    {
-      id: 2,
-      name: "Vishnu S",
-      designation: "General Secretary",
-      image:vishnu,
-      phone: "7012910210",
-      email: "hemanthsagar.mec@gmail.com",
-      linkedIn: "https://www.linkedin.com/in/hemanth-sagar-j-c-292a98195",
-    },
-    {
-      id: 3,
-      name: "Abhishek P",
-      designation: "Joint Secretary",
-      image: abhishek,
-      phone: "8921212340",
-      email: "sebindavis.mec@gmail.com",
-      linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
-    },
-    {
-      id: 4,
-      name: "Mayon Francis",
-      designation: "CS-Tech Manager",
-      image: mayon,
-      phone: "7306928074",
-      email: "saniamariasabu.mec@gmail.com",
-      linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
-    },
-    {
-      id: 5,
-      name: "Goutham C Arun",
-      designation: "Event Head",
-      image:goutham,
-      phone: "9400244505",
-      email: "gouthamchennamakkal@gmail.com",
-      linkedIn: "https://www.linkedin.com/in/goutham-c-arun-057b2722b/",
-    },
-    {
-      id: 6,
-      name: "Vaidyanath B",
-      designation: "Event Head",
-      image: vaidyanath,
-      phone: "8289986700",
-      email: "b.vaidyanath003@gmail.com ",
-      linkedIn: "https://in.linkedin.com/in/vaidyanath-b-b169a5258",
-    },
-  ];
-  
+  {
+    id: 1,
+    name: "Goutham C Arun",
+    designation: "Event Head",
+    image: goutham,
+    phone: "9400244505",
+    email: "gouthamchennamakkal@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/goutham-c-arun-057b2722b/",
+  },
+  {
+    id: 2,
+    name: "Vaidyanath B",
+    designation: "Event Head",
+    image: vaidyanath,
+    phone: "8289986700",
+    email: "b.vaidyanath003@gmail.com ",
+    linkedIn: "https://in.linkedin.com/in/vaidyanath-b-b169a5258",
+  },
+  {
+    id: 3,
+    name: "Mayon Francis",
+    designation: "CS-Tech Manager",
+    image: mayon,
+    phone: "7306928074",
+    email: "saniamariasabu.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/sania-maria-sabu-1b059a1b2/",
+  },
+  {
+    id: 4,
+    name: "Mathew Davis",
+    designation: "Chairperson",
+    image: mathew,
+    phone: "9605204433",
+    email: "nasreenkbasheer.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/nasreen-k-basheer-6795331a5/",
+  },
+  {
+    id: 5,
+    name: "Vishnu S",
+    designation: "General Secretary",
+    image: vishnu,
+    phone: "7012910210",
+    email: "hemanthsagar.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/hemanth-sagar-j-c-292a98195",
+  },
+  {
+    id: 6,
+    name: "Abhishek P",
+    designation: "Joint Secretary",
+    image: abhishek,
+    phone: "8921212340",
+    email: "sebindavis.mec@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/sebin-davis-344782195",
+  },
+];
